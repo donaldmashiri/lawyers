729ba867483822658081949610355de88178f052
Donald Mashiri <dondaldtondemashiri@gmail.com>
2024-05-31 08:09:10 +0200

    hj

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,7 @@
 
 <div class="container pb-3">
     <div class="row">
-        <h5 class="card-header">Student Registration</h5>
+        <h5 class="card-header">Lawyer Registration</h5>
         <div class="card-body">
             @include('layouts.messages')
             <form method="POST" action="{{ route('register') }}">
